3c2e495b1e0d6d2cb73e2cf20ebdc44ab1294d5b
abhisheksinha-989 <amansinha.989@gmail.com>
2026-09-10 11:30:20 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,14 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/abhisheksinha-989/refurbished-api/internal/config"
 )
 
 func main() {
+
+	cfg := config.MustLoad()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -17,13 +22,14 @@ func main() {
 	})
 
 	srv := http.Server{
-		Addr:         ":5050",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
 
+	log.Printf("server is listening on %s", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
