658b557ecad679ee5c7c5a081d40c205431bcac7
abhisheksinha-989 <amansinha.989@gmail.com>
2026-09-10 08:58:42 +0530

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("refurbished-api server is running!!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abhisheksinha-989/refurbished-api
+
+go 1.27.1
